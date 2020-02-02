3547c5b440a39d5ff2946983741d8149c82c716b
Italo Abreu <italodw@gmail.com>
2020-02-02 13:04:36 -0300

    gerando numero de reserva

--- source/estacionamento/controle/models.py
+++ source/estacionamento/controle/models.py
@@ -1,11 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Parking(models.Model):
-	license_plate = models.CharField(max_length=7)
-	reservation_number = models.CharField(max_length=7)
-	arrival = models.DateField()
-	departure = models.DateField()
+	plate = models.CharField(max_length=8, verbose_name='Placa do Veículo')
+	reservation =  models.CharField(
+		max_length=9, verbose_name='Número da Reserva')
+	arrival = models.DateTimeField(auto_now_add=True, verbose_name='Entrada')
+	departure = models.DateTimeField(
+		null=True, blank=True, verbose_name='Saída')
 
 	def __str__(self):
-		return f'{self.reservation_number} ({self.license_plate})'
+		return f'{self.reservation} ({self.plate})'
+
+	def make_reservation(self):
+		last = Parking.objects.filter(arrival__gte=timezone.now().date()).last()
+		prefix = 'RESV'
+
+		if not last:
+			return "{0}{1}".format(prefix, '001')
+
+		current_number = last.reservation.replace(prefix, '')
+		current_number = str(int(current_number) + 1)
+		if len(current_number) < 3:
+			current_number = ((3 - len(current_number)) * '0') + current_number
+
+		return "{0}{1}".format(prefix, current_number.zfill(3))
+
+	def save(self, *args, **kwargs):
+		if not self.reservation:
+			self.reservation = self.make_reservation()
+		super(Parking, self).save(*args, **kwargs)
--- source/estacionamento/controle/serializers.py
+++ source/estacionamento/controle/serializers.py
@@ -4,9 +4,4 @@ from .models import Parking
 class ParkingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Parking
-        fields = [
-			'license_plate',
-			'reservation_number',
-			'arrival',
-			'departure'
-        ]
+        fields = ['plate']
