79e90818ae264059612323d88cc42675f54ac04e
Italo Abreu <italodw@gmail.com>
2020-02-02 20:45:58 -0300

    pagamento implementado

--- source/estacionamento/controle/views.py
+++ source/estacionamento/controle/views.py
@@ -1,7 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from estacionamento.controle.models import Parking
 from estacionamento.controle.serializers import ParkingSerializer
 
 class ParkingViewSet(viewsets.ModelViewSet):
     queryset = Parking.objects.all().order_by('-arrival')
     serializer_class = ParkingSerializer
+
+    @action(detail=True, methods=['put'])
+    def paid(self, request, pk=None):
+        parking_to_paid = self.get_object()
+        serializer = ParkingSerializer
+        if parking_to_paid:
+            parking_to_paid.paid = True
+            parking_to_paid.save()
+            return Response({
+                'id': parking_to_paid.id,
+                'paid': True
+            })
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
